ddbe1c46afaeb85fb7f3d6597bf8b7fbda017e87
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-04-30 15:05:19 +0530

    Navbar animation

--- src/app/_components/Extras/Navbar.tsx
+++ src/app/_components/Extras/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ShoppingCart, User } from "lucide-react";
@@ -8,6 +8,10 @@ import Link from "next/link";
 export default function Navbar() {
     const [scrolled, setScrolled] = useState(false);
     const navRef = useRef(null);
+    const shopRef = useRef(null);
+    const philosophyRef = useRef(null);
+    const galleryRef = useRef(null);
+    const journalRef = useRef(null);
     const lastScrollY = useRef(0);
 
     useGSAP(() => {
@@ -38,26 +42,75 @@ export default function Navbar() {
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
+    // Underline On
+    const handleShopOn = () => {
+        gsap.to(shopRef.current, {
+            left: 0,
+            duration: 0.3,
+        });
+    }
+    const handlePhiOn = () => {
+        gsap.to(philosophyRef.current, {
+            left: 0,
+            duration: 0.3,
+        });
+    }
+    const handleGalleryOn = () => {
+        gsap.to(galleryRef.current, {
+            left: 0,
+            duration: 0.3,
+        });
+    }
+    const handleJournalOn = () => {
+        gsap.to(journalRef.current, {
+            left: 0,
+            duration: 0.3,
+        });
+    }
+
+    // Underline Off
+    const handleShopOff = () => {
+        gsap.to(shopRef.current, {
+            left: '-100%',
+            duration: 0.3,
+        });
+    }
+    const handlePhiOff = () => {
+        gsap.to(philosophyRef.current, {
+            left: '-100%',
+            duration: 0.3,
+        });
+    }
+    const handleGalleryOff = () => {
+        gsap.to(galleryRef.current, {
+            left: '-100%',
+            duration: 0.3,
+        });
+    }
+    const handleJournalOff = () => {
+        gsap.to(journalRef.current, {
+            left: '-100%',
+            duration: 0.3,
+        });
+    }
+
     return (
-        <nav
-            ref={navRef}
-            className={`fixed z-50 px-20 flex justify-between items-center w-full h-[4.5rem] transition-colors duration-300 ${scrolled ? "bg-zinc-50 text-zinc-700" : "text-zinc-50"}`}
-        >
+        <nav ref={navRef} className={`fixed z-50 px-20 flex justify-between items-center w-full h-[4.5rem] transition-colors duration-300 ${scrolled ? "bg-zinc-50 text-zinc-700 shadow" : "text-zinc-50"}`}>
             <Link href="/" className="w-32">
                 <h1 className="font-playball text-[1.7rem]">Zolivine</h1>
             </Link>
-            <ul className="flex text-xs gap-8 tracking-wider">
-                <li><Link href="/">SHOP</Link></li>
-                <li><Link href="/">PHILOSOPHY</Link></li>
-                <li><Link href="/">GALLERY</Link></li>
-                <li><Link href="/">JOURNAL</Link></li>
+            <ul className="flex text-[10px] font-semibold gap-12 tracking-wider">
+                <li onMouseEnter={handleShopOn} onMouseLeave={handleShopOff} className="relative overflow-hidden"><Link href="/shop">SHOP<span ref={shopRef} className="w-full absolute left-[-100%] bottom-0 h-[1px] bg-zinc-50"></span></Link></li>
+                <li onMouseEnter={handlePhiOn} onMouseLeave={handlePhiOff} className="relative overflow-hidden"><Link href="/philosophy">PHILOSOPHY<span ref={philosophyRef} className="w-full absolute left-[-100%] bottom-0 h-[1px] bg-zinc-50"></span></Link></li>
+                <li onMouseEnter={handleGalleryOn} onMouseLeave={handleGalleryOff} className="relative overflow-hidden"><Link href="/gallery">GALLERY<span ref={galleryRef} className="w-full absolute left-[-100%] bottom-0 h-[1px] bg-zinc-50"></span></Link></li>
+                <li onMouseEnter={handleJournalOn} onMouseLeave={handleJournalOff} className="relative overflow-hidden"><Link href="/journal">JOURNAL<span ref={journalRef} className="w-full absolute left-[-100%] bottom-0 h-[1px] bg-zinc-50"></span></Link></li>
             </ul>
             <div className={`w-32 rounded-4xl h-12 px-5 items-center ${scrolled ? "bg-zinc-700" : "bg-zinc-50"} transition-colors duration-300 flex justify-around`}>
                 <button className="cursor-pointer">
                     <ShoppingCart strokeWidth={1} className={`w-5 ${scrolled ? "text-zinc-50" : "text-zinc-700"} transition-colors duration-300`} />
                 </button>
                 <div className="h-5 border-l border-zinc-400"></div>
-                <Link href="/">
+                <Link href="/login">
                     <User strokeWidth={1} className={`w-5 ${scrolled ? "text-zinc-50" : "text-zinc-700"} transition-colors duration-300`} />
                 </Link>
             </div>
