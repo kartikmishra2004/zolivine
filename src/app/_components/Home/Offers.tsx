56209076dda77c12aedf4382d5b3e01e1146187b
Kartik Mishra <kartik2611mishra@gmail.com>
2025-06-01 21:44:48 +0530

    Offers section animations

--- src/app/_components/Home/Offers.tsx
+++ src/app/_components/Home/Offers.tsx
@@ -15,11 +15,34 @@ export default function Offers() {
 
     const screenRef = useRef(null);
     const imageRef = useRef(null);
-    const animated = useAnimation((s) => s.hasAnimated('home-journal'));
+    const title1Ref = useRef(null);
+    const title2Ref = useRef(null);
+    const animated = useAnimation((s) => s.hasAnimated('home-offers'));
     const setAnimated = useAnimation((s) => s.setAnimated);
     const [arrowHovered, setArrowHovered] = useState(false);
 
     useGSAP(() => {
+
+        if (!animated) {
+            gsap.from(title1Ref.current, {
+                scrollTrigger: {
+                    trigger: screenRef.current,
+                    start: '-65%',
+                },
+                y: 60,
+            });
+
+            gsap.from(title2Ref.current, {
+                scrollTrigger: {
+                    trigger: screenRef.current,
+                    start: '-65%',
+                },
+                y: 60,
+                delay: 0.2,
+                onComplete: () => setAnimated('home-offers', true)
+            });
+        }
+
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: screenRef.current,
@@ -35,7 +58,7 @@ export default function Offers() {
     });
 
     return (
-        <section ref={screenRef} className="w-full h-[125vh] flex relative overflow-hidden">
+        <section ref={screenRef} className="w-full h-[120vh] flex relative overflow-hidden">
             <div className="w-[50.1%] h-full flex flex-col justify-end absolute left-0 inset-0 bg-zinc-50 z-10">
                 <Image className="select-none ml-20" src={'/images/hanna-balan-d1GwyeOlZDw-unsplash.jpg'} width={230} height={230} alt="image"></Image>
                 <div className="h-1/2 w-full p-20">
@@ -49,10 +72,10 @@ export default function Offers() {
                     <div className="h-[300px] flex justify-between w-[49.9vw] p-12">
                         <div className="">
                             <div className="overflow-hidden">
-                                <h1 className="text-6xl font-semibold text-zinc-700">EXCITING</h1>
+                                <h1 ref={title1Ref} className="text-6xl font-semibold text-zinc-700">EXCITING</h1>
                             </div>
                             <div className="overflow-hidden">
-                                <h1 className="text-6xl font-semibold text-zinc-700">OFFERS <span className={`${EditorialNew.className} font-normal`}>awaits</span></h1>
+                                <h1 ref={title2Ref} className="text-6xl font-semibold text-zinc-700">OFFERS <span className={`${EditorialNew.className} font-normal`}>awaits</span></h1>
                             </div>
                             <div className="overflow-hidden">
                                 <p className="mt-4 text-sm text-zinc-600 leading-4 w-2/3" data-v-7cc9f5eb="">Shop now to get a chance to win 2 extra products. Grab the offer before it ends.</p>
